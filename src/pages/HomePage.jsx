b6166062096bc07ca895a61cdd72296c327572ca
JhonatanDczel <jariasq@unsa.edu.pe>
2024-07-21 23:24:11 -0500

    Componentes para los logos

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,13 @@
 import { LoginForm } from "../components/auth/Login";
+import Logo from "../components/common/Logo";
 
 function HomePage() {
   return (
     <div className="bg-dojo-day bg-cover bg-center w-screen h-screen">
+      <div>
+        <Logo path="/src/assets/logos/CoderDojo.png" />
+        <Logo path="/src/assets/logos/IEEE-CS-UNSA.png" />
+      </div>
       <LoginForm />
     </div>
   );
