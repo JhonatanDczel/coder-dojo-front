84689f9f9f8958e6e31ad52864d4494417f3d148
JhonatanDczel <jariasq@unsa.edu.pe>
2024-07-21 20:25:57 -0500

    Inicio de proyecto

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <div>
-      <h1 className="bg-purple-500 p-10 w-1/2">
+      <h1 className="bg-purple-500 font-bold text-3xl text-slate-300 p-10 w-1/2">
         Hola, prueba de componente
       </h1>
     </div>
