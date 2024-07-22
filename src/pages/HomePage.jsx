8771eabf4220655956748a639ad7c7fcfc64a854
rikich3 <rchambillap@unsa.edu.pe>
2024-07-22 08:51:44 -0500

    actualizando

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -50,11 +50,11 @@ function HomePage() {
             </div>
           </div>
         <div className="absolute top-5 left-5 p-4">
-          <Logo path="/src/assets/IEEE-CS-UNSA.png" />
+          <Logo path="/assets/IEEE-CS-UNSA.png" />
         </div>
         <a href="https://coderdojo.com/en/" target="_blank" className="inset-1">
           <div className="absolute top-5 right-5 p-4">
-            <Logo path="/src/assets/CoderDojo.png" />
+            <Logo path="/assets/CoderDojo.png" />
           </div>
         </a>
           <div className="absolute bottom-5 left-5 p-4">
