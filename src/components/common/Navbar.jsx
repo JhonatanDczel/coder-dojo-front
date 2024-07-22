60b1fe25865314e7ded92575583c36d66973da67
L0rD1ego <diegoalejandrocarbajal@gmail.com>
2024-07-22 00:34:08 -0500

    creation of navbar

---
+++ src/components/common/Navbar.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { FaUserCircle, FaQuestionCircle } from 'react-icons/fa';
+
+const Navbar = () => {
+  return (
+    <div className="w-full bg-white shadow-md py-4 px-6 flex justify-between items-center fixed top-0 left-0 z-10">
+      <div className="text-2xl font-bold">CoderDojo</div>
+      <div className="flex items-center">
+        <button className="mr-4">
+          <FaQuestionCircle size={24} />
+        </button>
+        <div className="flex items-center">
+          <FaUserCircle size={24} className="mr-2" />
+          <span>USERNAME</span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
