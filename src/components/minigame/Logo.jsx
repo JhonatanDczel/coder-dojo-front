df2a4a693c425f8e5fadfa65c7dc5a59c26cbf19
JhonatanDczel <jariasq@unsa.edu.pe>
2024-07-22 02:40:40 -0500

    Footer component: minigame

--- src/components/minigame/Logo.jsx
+++ src/components/minigame/Logo.jsx
@@ -1,7 +1,7 @@
 import '../App.css'
 const Logo = () => {
   return (
-    <div id='logo' className='absolute p-4 top-0 left-0 lg:p-10 text-[40px] text-blue-400'>SharkType</div>
+    <div id='logo' className='absolute p-4 top-0 left-0 lg:p-10 text-[40px] text-blue-400'>DojoType</div>
   )
 }
 
